2cf3b046ff1ad563d3797ceae8d908e722536c2d
Sergio Morales García <moralesgarcia300@gmail.com>
2023-04-06 12:00:51 +0200

    Pantallas y log out

--- app/src/main/java/com/tfg/hosthotel/MainActivity.kt
+++ app/src/main/java/com/tfg/hosthotel/MainActivity.kt
@@ -2,10 +2,46 @@ package com.tfg.hosthotel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.menu_add -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, AddFragment())
+                        .commit()
+                    true
+                }
+                R.id.menu_profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ProfileFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        bottomNavigationView.selectedItemId = R.id.menu_home
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, HomeFragment())
+            .commit()
+
     }
 }
