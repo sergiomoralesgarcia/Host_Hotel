e1820d2c8d3906e71b28567c1bbd1d7aa6076ce8
Sergio Morales García <moralesgarcia300@gmail.com>
2023-04-13 10:16:45 +0200

    BottonSheet añadido

--- app/src/main/java/com/tfg/hosthotel/ProfileFragment.kt
+++ app/src/main/java/com/tfg/hosthotel/ProfileFragment.kt
@@ -8,7 +8,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
@@ -42,10 +46,17 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
         val signOutButton: Button = view.findViewById(R.id.btn_logout)
         signOutButton.setOnClickListener {
             signOut()
         }
+
+        val showBottomSheetButton: ImageButton = view.findViewById(R.id.btn_show_bottom_sheet)
+        showBottomSheetButton.setOnClickListener {
+            showBottomSheet()
+        }
+
         return view
     }
 
@@ -67,6 +78,10 @@ class ProfileFragment : Fragment() {
         dialog.show()
     }
 
+    private fun showBottomSheet() {
+        val bottomSheet = BottomSheetFragment()
+        bottomSheet.show(requireFragmentManager(), "BottomSheetDialog")
+    }
 
     companion object {
         @JvmStatic
@@ -77,5 +92,21 @@ class ProfileFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+}
+
+class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.bottom_sheet_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val closeButton: Button = view.findViewById(R.id.btn_save)
+        closeButton.setOnClickListener {
+            dismiss()
+        }
     }
 }
