123f21c503fbf6e655d56588cb638256ce806290
Sergio Morales García <moralesgarcia300@gmail.com>
2023-06-11 17:05:42 +0200

    Boton flotante reviews

--- app/src/main/java/com/tfg/hosthotel/DetailActivity.kt
+++ app/src/main/java/com/tfg/hosthotel/DetailActivity.kt
@@ -1,6 +1,9 @@
 package com.tfg.hosthotel
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 import com.tfg.hosthotel.databinding.ActivityDetailBinding
@@ -25,5 +28,28 @@ class DetailActivity : AppCompatActivity() {
         binding.txtCiudad.text = hotelCity
         binding.txtCalle.text = hotelStreet
         binding.txtInfo.text = hotelInfo
+
+        binding.fabAdd.setOnClickListener {
+            showReviewDialog()
+        }
+    }
+
+    private fun showReviewDialog() {
+        val builder = AlertDialog.Builder(this)
+        val dialogView = layoutInflater.inflate(R.layout.dialog_review, null)
+        builder.setView(dialogView)
+
+        val etReview = dialogView.findViewById(R.id.et_review) as EditText
+        val btnSubmit = dialogView.findViewById(R.id.btn_submit) as Button
+
+        val dialog = builder.create()
+        dialog.show()
+
+        btnSubmit.setOnClickListener {
+            val review = etReview.text.toString()
+            // Aquí puedes realizar acciones con la reseña (guardarla, enviarla, etc.)
+
+            dialog.dismiss()
+        }
     }
 }
