992cba0c656470d5c42ec8194f047996f6fb43c7
Sergio Morales García <moralesgarcia300@gmail.com>
2023-06-14 12:26:50 +0200

    Arreglo cambio de idioma

--- app/src/main/java/com/tfg/hosthotel/menus/ConfigActivity.kt
+++ app/src/main/java/com/tfg/hosthotel/menus/ConfigActivity.kt
@@ -8,41 +8,33 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.ContentProviderCompat.requireContext
 import com.tfg.hosthotel.MainActivity
 import com.tfg.hosthotel.R
 import java.util.*
 
 class ConfigActivity : AppCompatActivity() {
+
+    private lateinit var languageSpinner: Spinner
     private var selectedLanguage: String = "en" // idioma por defecto: inglés
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
-        setLocale(selectedLanguage)
 
-        val saveButton = findViewById<Button>(R.id.btn_save)
-        saveButton.setOnClickListener {
-            val sp = getSharedPreferences("SP", Context.MODE_PRIVATE)
-            val editor = sp.edit()
-            val swi = findViewById<Switch>(R.id.switch_theme)
+        // Inicializa el Spinner
+        languageSpinner = findViewById(R.id.spinner_language)
 
-            if (swi.isChecked) {
-                editor.putInt("Theme", 0)
-            } else {
-                editor.putInt("Theme", 1)
-            }
-            editor.apply()
-            setDayNight()
-            setLocale(selectedLanguage) // aplica el idioma seleccionado
+        // Obtiene el idioma seleccionado guardado en las preferencias compartidas
+        val sp = getSharedPreferences("SP", Context.MODE_PRIVATE)
+        selectedLanguage = sp.getString("Language", "en") ?: "en"
 
-            Toast.makeText(this, "Cambios aplicados", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            startActivity(intent)
+        // Establece la selección del Spinner según el idioma guardado
+        val languageIndex = when (selectedLanguage) {
+            "es" -> 1 // Español
+            else -> 0 // Inglés (por defecto)
         }
+        languageSpinner.setSelection(languageIndex)
 
-        val languageSpinner = findViewById<Spinner>(R.id.spinner_language)
         languageSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
                 if (view != null) {
@@ -57,29 +49,71 @@ class ConfigActivity : AppCompatActivity() {
             override fun onNothingSelected(parent: AdapterView<*>) {}
         }
 
+        val saveButton = findViewById<Button>(R.id.btn_save)
+        saveButton.setOnClickListener {
+            val swi = findViewById<Switch>(R.id.switch_theme)
+            val theme = if (swi.isChecked) 0 else 1
+
+            saveTheme(theme)
+            saveLanguage(selectedLanguage)
+
+            Toast.makeText(this, "Cambios aplicados", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
+
         // Llama a setDayNight() para establecer el tema correcto al iniciar la actividad
         setDayNight()
     }
 
-    fun setDayNight() {
+    override fun onResume() {
+        super.onResume()
+        // Restaura el tema y el idioma al volver a abrir la actividad
+        setDayNight()
+        setLocale(selectedLanguage)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Guarda el idioma seleccionado en las preferencias compartidas
+        saveLanguage(selectedLanguage)
+    }
+
+    private fun saveTheme(theme: Int) {
+        val sp = getSharedPreferences("SP", MODE_PRIVATE)
+        val editor = sp.edit()
+        editor.putInt("Theme", theme)
+        editor.apply()
+        setDayNight()
+    }
+
+    private fun setDayNight() {
         val sp = getSharedPreferences("SP", MODE_PRIVATE)
         val theme = sp.getInt("Theme", 1)
-        val editor = sp.edit()
 
         // Si el tema es claro, marca el interruptor como no seleccionado, de lo contrario, marca el interruptor como seleccionado
         val swi = findViewById<Switch>(R.id.switch_theme)
         swi.isChecked = (theme == 0)
 
-        if (theme == 0) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        val nightMode = when (theme) {
+            0 -> AppCompatDelegate.MODE_NIGHT_YES
+            else -> AppCompatDelegate.MODE_NIGHT_NO
         }
-        editor.putInt("Theme", theme)
-        editor.apply()
+
+        AppCompatDelegate.setDefaultNightMode(nightMode)
     }
 
-    fun setLocale(languageCode: String) {
+    private fun saveLanguage(languageCode: String) {
+        val sp = getSharedPreferences("SP", Context.MODE_PRIVATE)
+        val editor = sp.edit()
+        editor.putString("Language", languageCode)
+        editor.apply()
+        setLocale(languageCode)
+    }
+
+    private fun setLocale(languageCode: String) {
         val locale = Locale(languageCode)
         Locale.setDefault(locale)
         val config = Configuration()
@@ -87,4 +121,3 @@ class ConfigActivity : AppCompatActivity() {
         baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
     }
 }
-
