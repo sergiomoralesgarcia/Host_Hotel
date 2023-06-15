3c8f35b98275c1d528a039cf087387d1ccc13b9e
Sergio Morales García <moralesgarcia300@gmail.com>
2023-06-16 01:14:31 +0200

    Ultimos retoques

--- app/src/main/java/com/tfg/hosthotel/adapters/MyAdapter.kt
+++ app/src/main/java/com/tfg/hosthotel/adapters/MyAdapter.kt
@@ -62,25 +62,6 @@ class MyAdapter(
         holder.itemView.findViewById<View>(R.id.btn_edit)?.setOnClickListener {
             itemEditClickListener.onItemEditClick(currentItem)
         }
-
-        val favButton = holder.itemView.findViewById<ImageView>(R.id.btn_fav)
-        if (currentItem.isFavorite) {
-            favButton.setImageResource(R.drawable.ic_fav_on)
-        } else {
-            favButton.setImageResource(R.drawable.ic_fav_off)
-        }
-
-        // Dentro del bloque `onClickListener` de `favButton`
-        favButton.setOnClickListener {
-            currentItem.isFavorite = !currentItem.isFavorite
-            updateFavoriteStatus(currentItem, currentItem.isFavorite) // Pasar el estado de favorito como segundo argumento
-            if (currentItem.isFavorite) {
-                favButton.setImageResource(R.drawable.ic_fav_on)
-            } else {
-                favButton.setImageResource(R.drawable.ic_fav_off)
-            }
-        }
-
     }
 
     override fun getItemCount(): Int {
@@ -91,28 +72,5 @@ class MyAdapter(
         val name: TextView = itemView.findViewById(R.id.txt_name)
         val city: TextView = itemView.findViewById(R.id.txt_ciudad)
         val imageView: ImageView = itemView.findViewById(R.id.hotelImage)
-    }
-
-    private fun updateFavoriteStatus(hotel: Hotel, isFavorite: Boolean) {
-        val db = FirebaseFirestore.getInstance()
-        val hotelsCollection = db.collection("hotels")
-        val query = hotelsCollection.whereEqualTo("name_hotel", hotel.name_hotel)
-
-        query.get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    val hotelRef = hotelsCollection.document(document.id)
-                    hotelRef.update("isFavorite", isFavorite)
-                        .addOnSuccessListener {
-                            // El estado de favorito se actualizó correctamente en Firestore
-                        }
-                        .addOnFailureListener { exception ->
-                            // Error al actualizar el estado de favorito en Firestore
-                        }
-                }
-            }
-            .addOnFailureListener { exception ->
-                // Error al obtener los documentos de Firestore
-            }
     }
 }
